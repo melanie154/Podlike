14d9ffe605f1903e09d54b689d479ffe4272c513
CaroMinion <72865687+CaroMinion@users.noreply.github.com>
2020-12-10 16:58:40 +0100

    Add files via upload

--- podlike/src/main/java/de/gsohs/bme5/MyServer.java
+++ podlike/src/main/java/de/gsohs/bme5/MyServer.java
@@ -1,10 +1,18 @@
 package de.gsohs.bme5;
 import io.javalin.Javalin;
+import java.util.List;
+import java.util.Map;
+
+
 
 public class MyServer {
     public static void main(String[] args) {
         Javalin javalin = Javalin.create().start(8080);
         javalin.config.addStaticFiles("Media_Podlike");
-        javalin.get("/", ctx -> ctx.render("podlike_Hauptseite.html"));
+
+        javalin.get("/", ctx -> {
+            ctx.render("podlike_Hauptseite.html", Map.of("fruits", new String[]{"apples", "bananas", "peaches"}));
+        });
+
     }
 }
